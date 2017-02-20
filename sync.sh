99661965e2d755f040c7937a6962e26973898604
Zac Pustejovsky <zaqk.putsky@gmail.com>
2017-02-20 17:03:51 -0500

    adding music21 scrabpook notebook

--- sync.sh
+++ sync.sh
@@ -17,18 +17,27 @@ get_remote_files() {
         exit 1
     fi
     file=$1
+    base_file=`basename ${file}`
     auth_to_cluster
-    kubectl cp ${pod_name}:${persistent_data_path}/${file} ${local_data_path}/${file}
+    echoRun "kubectl cp ${pod_name}:${persistent_data_path}/${file} ${local_data_path}/${base_file}"
 }
 
+echoRun() {
+    echo "${yellow}> $1${reset}"
+    eval $1
+}
+
+
 copy_local_files() {
-    if [ $# -ne 1 ];
-        then echo "Must specify a file in ${local_data_path}"
+    if [ $# -ne 2 ];
+        then echo "Must specify source file in ${local_data_path} and a target directory in ${persistent_data_path}"
         exit 1
     fi
-    file=$1
+    source_file=$1
+    target_dir=$2
+    base_file=`basename ${source_file}`
     auth_to_cluster
-    kubectl cp ${local_data_path}/${file} ${pod_name}:${persistent_data_path}/${file}
+    echoRun "kubectl cp ${source_file} ${pod_name}:${persistent_data_path}/${target_dir}/"
 }
 
 if [ $# -eq 0 ];
@@ -41,7 +50,7 @@ then
     get_remote_files $2
 elif [ $1 = "push" ]
 then
-    copy_local_files $2
+    copy_local_files $2 $3
 else
     echo "${usage}"
 fi
