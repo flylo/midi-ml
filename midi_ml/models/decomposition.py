82690d1d6cbdec3abe9252b0006ffb4bf94e6613
Zac Pustejovsky <zaqk.putsky@gmail.com>
2017-04-04 11:26:02 -0400

    Adding SVD and num_components options to PCA

--- midi_ml/models/decomposition.py
+++ midi_ml/models/decomposition.py
@@ -1,25 +1,35 @@
 import numpy as np
 
+
 # TODO: docstrings
 class PrincipalComponents(object):
     """
     Run a principal components analysis
     """
+    _methods = ["eigendecomposition", "svd"]
 
     def __init__(self, X,
+                 method="eigendecomposition",
                  regularization: float = None,
-                 keep_copy_of_X: bool = True,):
+                 num_components: float = None,
+                 keep_copy_of_X: bool = True, ):
         """
         :param X: Input data
         :param regularization: (optional) whether we want to regularize the covariance matrix to avoid singularity
+        :param num_components: (optional) the number of PCs to keep
         :param keep_copy_of_X: whether to keep a copy of the training data once we train
         """
         self.X = X.astype(float)
+        if method in self._methods:
+            self.method_ = method
+        else:
+            raise ValueError("Must choose method in %s" % self._methods)
         self.means_ = None  # type: np.array
         self.sds_ = None  # type: np.array
         self.covariance_ = None  # type: np.array
         self.eigenvalues_ = None  # type: np.array
         self.projection_matrix_ = None  # type: np.array
+        self.num_components_ = num_components
         self.num_records_ = self.X.shape[0]
         self.regularization_ = regularization
         self.keep_copy_of_X = keep_copy_of_X
@@ -65,6 +75,16 @@ class PrincipalComponents(object):
         self.eigenvalues_ = eigenvalues[eigenvalue_order]
         self.projection_matrix_ = eigendecomposition[:, eigenvalue_order]
 
+    def _svd(self):
+        """
+
+        :return:
+        """
+        u, s, v = np.linalg.svd(self.X, full_matrices=False)
+        self.eigenvalues_ = s**2 / self.X.shape[0]
+        # self.projection_matrix_ = v.T
+        self.projection_matrix_ = v.T
+
     def transform(self, new_X: np.array = None) -> np.array:
         if self.projection_matrix_ is None:
             raise ValueError("Must fit model before transformation")
@@ -78,8 +98,14 @@ class PrincipalComponents(object):
 
     def fit(self):
         self._normalize()
-        self._get_covariance()
-        self._eigendecomposition()
+        if self.method_ == "eigendecomposition":
+            self._get_covariance()
+            self._eigendecomposition()
+        else:
+            self._svd()
+        if self.num_components_ is not None:
+            self.eigenvalues_ = self.eigenvalues_[:self.num_components_]
+            self.projection_matrix_ = self.projection_matrix_[:,:self.num_components_]
         if not self.keep_copy_of_X:
             self.X = None
 
