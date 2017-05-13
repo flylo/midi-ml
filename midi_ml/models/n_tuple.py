117496d78b49557bb00a8a3937374f251734319c
Zac Pustejovsky <zaqk.putsky@gmail.com>
2017-05-13 19:13:11 -0400

    working search that optimizes out-of-sample

--- midi_ml/models/n_tuple.py
+++ midi_ml/models/n_tuple.py
@@ -30,7 +30,9 @@ class NTupleClassifier(object):
         # class-conditional pattern-conditional contingency table
         self.T = {}
         # must be integers starting at 0 and increasing by 1
-        self.classes_ = set(self.y)
+        classes, priors = np.unique(y, return_counts=True)
+        self.classes_ = classes
+        self.priors = priors / priors.sum()
         self.keep_copy_of_X = keep_copy_of_X
         self.num_bins = num_bins
         self.X_discretized = None  # type: np.array
@@ -40,7 +42,7 @@ class NTupleClassifier(object):
         # new attributes as per convo with haralick
         self.nd_contingency_table = {}
 
-    def predict(self, new_X: np.array = None):
+    def predict(self, new_X: np.array = None, log_likelihood: bool = False):
         """
         Exposed API to make predictions
         :param new_X: New set of X values to make predictions with (optional)
@@ -62,10 +64,6 @@ class NTupleClassifier(object):
                             T_hat[cls][i, m] = self.T[m][cls][x_subspace]
                         except KeyError:
                             continue
-            log_prob = np.vstack([np.log(T_hat[cls] + 0.00001).sum(axis=1) for cls in sorted_classes]).T
-            return log_prob.argmax(axis=1)
-
-
         else:
             new_X_discretized = self._discretize_prediction_data(new_X)
             T_hat = {cls: np.zeros((new_X_discretized.shape[0], self.M)) for cls in self.classes_}
@@ -77,7 +75,13 @@ class NTupleClassifier(object):
                             T_hat[cls][i, m] = self.T[m][cls][x_subspace]
                         except KeyError:
                             continue
-            log_prob = np.vstack([np.log(T_hat[cls] + 0.00001).sum(axis=1) for cls in sorted_classes]).T
+
+        log_prob = (np.vstack([np.log(T_hat[cls] + 0.00001).sum(axis=1) for cls in sorted_classes]).T +
+                    np.log(self.priors))
+        # log_prob = log_prob + np.log(self.priors)
+        if log_likelihood:
+            return log_prob
+        else:
             return log_prob.argmax(axis=1)
 
     def _discretize_prediction_data(self, new_X: np.array = None) -> np.array:
@@ -135,59 +139,80 @@ class NTupleClassifier(object):
                 self.T[m][cls] = {k: v / num_records_in_class for k, v in pattern_hash_table.items()}
         self.fitted = True
 
-    def _pattern_log_prob(self, candidate_pattern: tuple, candidate_T_m: dict):
-        prob_data_given_class = np.zeros((self.X_discretized.shape[0], len(self.classes_))) + 0.00001
-        for i, x in enumerate(self.X_discretized):
-            x_subspace = tuple(x[candidate_pattern])
-            for cls in self.classes_:
-                try:
-                    prob_data_given_class[i, cls] = candidate_T_m[cls][x_subspace]
-                except KeyError:
-                    continue
-        # try this bayes rule weee
-        priors = np.bincount(self.y) / len(self.y)
-        normalizing_constant = (prob_data_given_class * priors).sum(axis=1)
-        # pdb.set_trace()
-        log_prob = np.log(prob_data_given_class * priors / np.vstack([normalizing_constant for _ in self.classes_]).T)
-        #
-        return log_prob
+    def _get_predictions(self,
+                         X_test: np.array,
+                         m: int = None,
+                         candidate_pattern: tuple = None,
+                         candidate_T_m: dict = None):
+        if m is None and candidate_pattern is None and candidate_T_m is None:
+            # current log prob of data given class
+            return self.predict(X_test, log_likelihood=False)
+        elif m is not None and candidate_pattern is not None and candidate_T_m is not None:
+            original_pattern = self.pattern_sets[m].copy()
+            original_T_m = self.T[m].copy()
+            self.pattern_sets[m] = candidate_pattern
+            self.T[m] = candidate_T_m
+            preds = self.predict(X_test, log_likelihood=False)
+            self.pattern_sets[m] = original_pattern
+            self.T[m] = original_T_m
+            return preds
+        else:
+            return AttributeError("invalid usage")
 
-    def _train_new_pattern_set(self, m: int):
+    def _train_new_pattern_set(self, num_folds: int, m: int):
+        kfold = KFold(n_splits=num_folds)
+        scores_candidate = []
+        scores_orig = []
         possible_dimensions = np.arange(self.X.shape[1])
         candidate_pattern = self.pattern_sets[m].copy()
-        # print("original pattern: " + str(self.pattern_sets[m]))
         # randomly select an element to switch and randomly insert a new element
-        candidate_pattern[self.random_state.choice(self.L,)] = self.random_state.choice(possible_dimensions)
-        # print("new pattern: " + str(candidate_pattern))
-        candidate_T_m = {}
-        for cls in self.classes_:
-            cls_idx = np.where(self.y == cls)[0]
-            num_records_in_class = cls_idx.shape[0]
-            candidate_T_m[cls] = {}
-            for row in cls_idx:
-                x = self.X_discretized[row, :]
-                x_subspace = tuple(x[candidate_pattern])
-                try:
-                    candidate_T_m[cls][x_subspace] += 1
-                except KeyError:
-                    candidate_T_m[cls][x_subspace] = 1
-            candidate_T_m[cls] = {k: v / num_records_in_class for k, v in candidate_T_m[cls].items()}
-        candidate_log_prob = self._pattern_log_prob(candidate_pattern, candidate_T_m)
-        original_log_prob = self._pattern_log_prob(self.pattern_sets[m], self.T[m])
-        class_mask = np.stack([self.y == cls for cls in range(len(self.classes_))], axis=1)
-        new_cross_entropy = -candidate_log_prob[class_mask].sum()
-        original_cross_entropy = -original_log_prob[class_mask].sum()
-        # pdb.set_trace()
-        # print("original cross ent: " + str(original_cross_entropy))
-        # print("new cross ent: " + str(new_cross_entropy))
-        if new_cross_entropy < original_cross_entropy:
-            # print("new pattern selected!")
+        candidate_pattern[self.random_state.choice(self.L, )] = self.random_state.choice(possible_dimensions)
+        for train_idx, test_idx in kfold.split(self.y):
+            X_train = self._discretize_prediction_data(self.X[train_idx])
+            X_test = self.X[test_idx]
+            y_train = self.y[train_idx]
+            y_test = self.y[test_idx]
+            # print("new pattern: " + str(candidate_pattern))
+            candidate_T_m = {}
+            for cls in self.classes_:
+                cls_idx = np.where(y_train == cls)[0]
+                num_records_in_class = cls_idx.shape[0]
+                candidate_T_m[cls] = {}
+                for row in cls_idx:
+                    x = X_train[row, :]
+                    x_subspace = tuple(x[candidate_pattern])
+                    try:
+                        candidate_T_m[cls][x_subspace] += 1
+                    except KeyError:
+                        candidate_T_m[cls][x_subspace] = 1
+                candidate_T_m[cls] = {k: v / num_records_in_class for k, v in candidate_T_m[cls].items()}
+            candidate_preds = self._get_predictions(X_test, m, candidate_pattern, candidate_T_m)
+            original_preds = self._get_predictions(X_test)
+            candidate_accuracy = 1. - np.count_nonzero(np.subtract(candidate_preds, y_test))/len(y_test)
+            original_accuracy = 1. - np.count_nonzero(np.subtract(original_preds, y_test))/len(y_test)
+            scores_candidate.append(candidate_accuracy)
+            scores_orig.append(original_accuracy)
+        if np.mean(scores_candidate) > np.mean(scores_orig):
+            print("new pattern selected!")
+            candidate_T_m = {}
+            for cls in self.classes_:
+                cls_idx = np.where(self.y == cls)[0]
+                num_records_in_class = cls_idx.shape[0]
+                candidate_T_m[cls] = {}
+                for row in cls_idx:
+                    x = self.X_discretized[row, :]
+                    x_subspace = tuple(x[candidate_pattern])
+                    try:
+                        candidate_T_m[cls][x_subspace] += 1
+                    except KeyError:
+                        candidate_T_m[cls][x_subspace] = 1
+                candidate_T_m[cls] = {k: v / num_records_in_class for k, v in candidate_T_m[cls].items()}
             self.pattern_sets[m] = candidate_pattern
             self.T[m] = candidate_T_m
 
-    def fit_optimize(self, num_iter: int = 10):
-        for i in range(num_iter):
-            self._train_new_pattern_set(self.random_state.choice(self.M))
+    def fit_optimize(self, num_iter: int = 100, num_folds: int = 10):
+        for _ in range(num_iter):
+            self._train_new_pattern_set(num_folds, self.random_state.choice(self.M))
 
 
 if __name__ == "__main__":
@@ -202,25 +227,18 @@ if __name__ == "__main__":
                                         random_state=1010)
     X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y)
 
-    nt = NTupleClassifier(X_train, y_train, M=20, L=2, num_bins=10)
+    nt = NTupleClassifier(X_train, y_train, M=5, L=2, num_bins=5)
     nt.fit_init()
     nt.predict()
-    # nt.fit_optimize(num_iter=100)
-    # preds = nt.predict(new_X=X_test)
-    print(metrics.accuracy_score(y_train, nt.predict()))
+    print(metrics.accuracy_score(y_test, nt.predict(new_X=X_test)))
+    nt.fit_optimize(num_iter=100, num_folds=5)
+    print(metrics.accuracy_score(y_test, nt.predict(new_X=X_test)))
+    #
+    # from sklearn.linear_model import LogisticRegression
+    #
+    # lr = LogisticRegression()
+    # lr.fit(X_train, y_train)
+    # preds = lr.predict(X_test)
+    # print(metrics.confusion_matrix(y_train, lr.predict(X_train)))
     # print(metrics.confusion_matrix(y_test, preds))
     # print(metrics.accuracy_score(y_test, preds))
-    nt.fit_optimize(num_iter=1000)
-    # preds = nt.predict(new_X=X_test)
-    print(metrics.accuracy_score(y_train, nt.predict()))
-
-
-
-    from sklearn.linear_model import LogisticRegression
-
-    lr = LogisticRegression()
-    lr.fit(X_train, y_train)
-    preds = lr.predict(X_test)
-    print(metrics.confusion_matrix(y_train, lr.predict(X_train)))
-    print(metrics.confusion_matrix(y_test, preds))
-    print(metrics.accuracy_score(y_test, preds))
